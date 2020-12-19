12aa11df3647f14e3779f5ab285db4eda136f4f5
jjs9000 <farriszamrie15@gmail.com>
2020-12-19 16:00:18 +0800

    completed Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,15 @@
 require('./bootstrap');
 
 import Vue from 'vue'
+import App from './components/app'
 
-import App from './vue/app'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 window.Vue = require('vue');
 
@@ -23,7 +30,8 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('app', require('./components/app.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -33,4 +41,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    components: {App}
 });
